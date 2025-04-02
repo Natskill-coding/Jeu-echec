3ff23ff9e38adf103a3136d8a959f35319eca36e
Nathan <nathansfert@gmail.com>
2025-04-02 18:20:04 +0200

    Déplacement basique de pièces

--- code/fonctions.py
+++ code/fonctions.py
@@ -1,14 +1,20 @@
 import pygame
+import math
 from piece_class import *
 
 
 def create_pieces():
-    tour_noir = Piece("black", "tour", pygame.transform.scale(pygame.image.load("./ressources/pieces/tour_noir_1.png").convert(), (100, 100)), (1 * 50, 1 * 50))
+    tour_noir_1 = Piece("black", "tour", pygame.transform.scale(pygame.image.load("./ressources/pieces/tour_noir_1.png").convert(), (100, 100)), (0, 0))
+    cavalier_noir_1 = Piece("black", "cavalier", pygame.transform.scale(pygame.image.load("./ressources/pieces/cavalier_noir_1.png").convert(), (100, 100)), (100, 0))
+    fou_noir_1 = Piece("black", "fou", pygame.transform.scale(pygame.image.load("./ressources/pieces/fou_noir_1.png").convert(), (100, 100)), (200, 0))
+    dame_noir_1 = Piece("black", "dame", pygame.transform.scale(pygame.image.load("./ressources/pieces/dame_noir_1.png").convert(), (100, 100)), (300, 0))
+    roi_noir_1 = Piece("black", "roi", pygame.transform.scale(pygame.image.load("./ressources/pieces/roi_noir_1.png").convert(), (100, 100)), (400, 0))
 
     echiquier = [
-        [tour_noir]
+        [tour_noir_1, cavalier_noir_1, fou_noir_1, dame_noir_1, roi_noir_1]
     ]
     return echiquier
+
 
 def detect_click_on_piece(echiquier, mouse_pos):
     for y, row in enumerate(echiquier):
@@ -17,3 +23,53 @@ def detect_click_on_piece(echiquier, mouse_pos):
             y_intervalle = (piece.position[1], piece.position[1] + 100)
             if (x_intervalle[0] <= mouse_pos[0] <= x_intervalle[1]) and (y_intervalle[0] <= mouse_pos[1] <= y_intervalle[1]):
                 return piece
+
+
+def is_mouvement_correct(pos_initial, pos_final, piece_type):
+    pos_initial_tab = ((pos_initial[0] / 100), (pos_initial[1] / 100))
+    pos_final_tab_rounded = (math.floor(pos_final[0] / 100), math.floor(pos_final[1] / 100))
+
+    if piece_type == "tour":
+        if pos_initial_tab[0] == pos_final_tab_rounded[0] or pos_initial_tab[1] == pos_final_tab_rounded[1]:
+            return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
+        
+    if piece_type == "cavalier":
+        if pos_final_tab_rounded[0] - pos_initial_tab[0] == 2:
+            if pos_final_tab_rounded[1] - pos_initial_tab[1] == 1:
+                return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
+            if pos_final_tab_rounded[1] - pos_initial_tab[1] == -1:
+                return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
+            
+        if pos_final_tab_rounded[0] - pos_initial_tab[0] == -2:
+            if pos_final_tab_rounded[1] - pos_initial_tab[1] == 1:
+                return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
+            if pos_final_tab_rounded[1] - pos_initial_tab[1] == -1:
+                return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
+            
+        if pos_final_tab_rounded[1] - pos_initial_tab[1] == 2:
+            if pos_final_tab_rounded[0] - pos_initial_tab[0] == 1:
+                return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
+            if pos_final_tab_rounded[0] - pos_initial_tab[0] == -1:
+                return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
+            
+        if pos_final_tab_rounded[1] - pos_initial_tab[1] == -2:
+            if pos_final_tab_rounded[0] - pos_initial_tab[0] == 1:
+                return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
+            if pos_final_tab_rounded[0] - pos_initial_tab[0] == -1:
+                return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
+            
+    if piece_type == "fou":
+        if abs(pos_final_tab_rounded[0] - pos_initial_tab[0]) == abs(pos_final_tab_rounded[1] - pos_initial_tab[1]):
+            return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
+        
+    if piece_type == "dame":
+        dx = abs(pos_final_tab_rounded[0] - pos_initial_tab[0])
+        dy = abs(pos_final_tab_rounded[1] - pos_initial_tab[1])
+        if dx == 0 or dy == 0 or dx == dy:
+            return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
+        
+    if piece_type == "roi":
+        dx = abs(pos_final_tab_rounded[0] - pos_initial_tab[0])
+        dy = abs(pos_final_tab_rounded[1] - pos_initial_tab[1])
+        if dx <= 1 and dy <= 1:
+            return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
--- code/script.py
+++ code/script.py
@@ -11,27 +11,51 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 
-BG_IMAGE = pygame.transform.scale(pygame.image.load("./ressources/echiquier.jpg").convert(), (SCREEN_WIDTH, SCREEN_HEIGHT))
+BG_IMAGE = pygame.transform.scale(pygame.image.load("./ressources/echiquier.png").convert(), (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 echiquier = create_pieces()
 
 
 is_running = True
-white_black = 1
+# white_black = 1
+piece_clicked = None
+piece_clicked_pos_initial = None
+offset_x = 0
+offset_y = 0
 
 while is_running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
+
         if event.type == pygame.MOUSEBUTTONDOWN:
-            pass
+            piece_clicked = detect_click_on_piece(echiquier, event.pos)
+            if piece_clicked:
+                piece_clicked_pos_initial = piece_clicked.position
+                offset_x =  event.pos[0] - piece_clicked.position[0]
+                offset_y = event.pos[1] - piece_clicked.position[1]
+        
+        elif event.type == pygame.MOUSEMOTION:
+            if piece_clicked:
+                piece_clicked.position = (event.pos[0] - offset_x, event.pos[1] - offset_y)
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if piece_clicked:
+                pos_final_correct = is_mouvement_correct(piece_clicked_pos_initial, event.pos, piece_clicked.type)
+                if pos_final_correct:
+                    piece_clicked.position = pos_final_correct
+                else:
+                    piece_clicked.position = piece_clicked_pos_initial
+            piece_clicked = None
+
 
     screen.blit(BG_IMAGE, (0, 0))
     for y, row in enumerate(echiquier):
         for x, piece in enumerate(row):
             screen.blit(piece.image, piece.position)
+
 
     pygame.display.flip()
     clock.tick(60)
