c6fa5667610885d6b607fc7be6ab029d12d18403
Nathan <nathansfert@gmail.com>
2025-04-04 18:08:33 +0100

    Cela avance

--- code/fonctions.py
+++ code/fonctions.py
@@ -23,6 +23,11 @@ def detect_click_on_piece(echiquier, mouse_pos):
             y_intervalle = (piece.position[1], piece.position[1] + 100)
             if (x_intervalle[0] <= mouse_pos[0] <= x_intervalle[1]) and (y_intervalle[0] <= mouse_pos[1] <= y_intervalle[1]):
                 return piece
+            
+
+def pieces_same_x_y_plus_proches(list, key):
+    piece_valeur_min = min(list, key=key)
+    return [piece for piece in list if key(piece) == key(piece_valeur_min)]
 
 
 def is_mouvement_correct(pos_initial, pos_final, piece_type, echiquier):
@@ -34,16 +39,18 @@ def is_mouvement_correct(pos_initial, pos_final, piece_type, echiquier):
         for x, piece in enumerate(row):
             list_pieces.append(piece)
 
+
     if piece_type == "tour":
         if pos_initial_tab[0] == pos_final_tab_rounded[0]:
             list_piece_same_x = list(filter(lambda piece: piece.position[0] / 100 == pos_initial_tab[0] and piece.position[1] / 100 != pos_initial_tab[1], list_pieces))
             if list_piece_same_x:
-                for piece in list_piece_same_x:
-                    if piece.position[1] / 100 > pos_initial_tab[1]:
-                        if pos_final_tab_rounded[1] < piece.position[1] / 100:
+                pieces_same_y_plus_proches = pieces_same_x_y_plus_proches(list_piece_same_x, key=lambda piece: abs(piece.position[1] / 100 - pos_initial_tab[1]))
+                if len(pieces_same_y_plus_proches) == 1:
+                    if pieces_same_y_plus_proches[0].position[1] / 100 > pos_initial_tab[1]:
+                        if pos_final_tab_rounded[1] < pieces_same_y_plus_proches[0].position[1] / 100:
                             return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
-                    elif piece.position[1] / 100 < pos_initial_tab[1]:
-                        if pos_final_tab_rounded[1] > piece.position[1] / 100:
+                    elif pieces_same_y_plus_proches[0].position[1] / 100 < pos_initial_tab[1]:
+                        if pos_final_tab_rounded[1] > pieces_same_y_plus_proches[0].position[1] / 100:
                             return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
             else: 
                 return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
@@ -51,12 +58,13 @@ def is_mouvement_correct(pos_initial, pos_final, piece_type, echiquier):
         if pos_initial_tab[1] == pos_final_tab_rounded[1]:
             list_piece_same_y = list(filter(lambda piece: piece.position[1] / 100 == pos_initial_tab[1] and piece.position[0] / 100 != pos_initial_tab[0], list_pieces))
             if list_piece_same_y:
-                for piece in list_piece_same_y:
-                    if piece.position[0] / 100 > pos_initial_tab[0]:
-                        if pos_final_tab_rounded[0] < piece.position[0] / 100:
+                pieces_same_x_plus_proches = pieces_same_x_y_plus_proches(list_piece_same_y, key=lambda piece: abs(piece.position[0] / 100 - pos_initial_tab[0]))
+                if len(pieces_same_x_plus_proches) == 1:
+                    if pieces_same_x_plus_proches[0].position[0] / 100 > pos_initial_tab[0]:
+                        if pos_final_tab_rounded[0] < pieces_same_x_plus_proches[0].position[0] / 100:
                             return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
-                    elif piece.position[0] / 100 < pos_initial_tab[0]:
-                        if pos_final_tab_rounded[0] > piece.position[0] / 100:
+                    elif pieces_same_x_plus_proches[0].position[0] / 100 < pos_initial_tab[0]:
+                        if pos_final_tab_rounded[0] > pieces_same_x_plus_proches[0].position[0] / 100:
                             return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
             else: 
                 return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
@@ -87,9 +95,15 @@ def is_mouvement_correct(pos_initial, pos_final, piece_type, echiquier):
             if pos_final_tab_rounded[0] - pos_initial_tab[0] == -1:
                 return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
             
+
     if piece_type == "fou":
         if abs(pos_final_tab_rounded[0] - pos_initial_tab[0]) == abs(pos_final_tab_rounded[1] - pos_initial_tab[1]):
+            # print(f"Initial: {pos_initial_tab}, Final: {pos_final_tab_rounded}")
+            pieces_same_diagonale = list(filter(lambda piece: abs(pos_initial_tab[0] - piece.position[0] / 100) == abs(pos_initial_tab[1] - piece.position[1] / 100), list_pieces))
+            # for piece in pieces_same_diagonale:
+            #     print(f"Même diagonale : {piece.type}, Position: {piece.position}")
             return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
+
         
     if piece_type == "dame":
         dx = abs(pos_final_tab_rounded[0] - pos_initial_tab[0])
@@ -97,6 +111,7 @@ def is_mouvement_correct(pos_initial, pos_final, piece_type, echiquier):
         if dx == 0 or dy == 0 or dx == dy:
             return (pos_final_tab_rounded[0] * 100, pos_final_tab_rounded[1] * 100)
         
+
     if piece_type == "roi":
         dx = abs(pos_final_tab_rounded[0] - pos_initial_tab[0])
         dy = abs(pos_final_tab_rounded[1] - pos_initial_tab[1])
